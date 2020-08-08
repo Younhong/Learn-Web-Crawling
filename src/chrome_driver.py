f4816906efbcef13d61fa0c337c56601d3a29b37
younhong <younhong@kakao.com>
2020-08-08 16:07:55 +0900

    Create chrome_driver.py

---
+++ src/chrome_driver.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome("C:/Users/21400/Desktop/chromedriver.exe")
+browser.get("http://python.org")
+
+menus = browser.find_elements_by_css_selector('#top ul.menu li')
+pypi = None
+for m in menus:
+    if m.text == "PyPI":
+        pypi = m
+        print(m.text)
+pypi.click() # 클릭
+time.sleep(5) # 5초 대기
+browser.quit()
